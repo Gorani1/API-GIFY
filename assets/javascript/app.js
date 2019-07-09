e5c3434fc25c6bf5832960f99bd6543bebacbcf8
Gorani1 <us.gorani@gmail.com>
2019-07-09 14:09:03 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,44 @@
 $(document).ready(function(){
-    let foods = ["cheese", "beef", "salads", "tacos"];
+    var foods = ["cheese", "beef", "salads", "tacos"];
 
 function populateButtons(arrayToUse, classToAdd, areaToAddTo){
     $(areaToAddTo).empty();
+    console.log (arrayToUse)
+    console.log (areaToAddTo)
     for (let i=0; i < arrayToUse.length; i++){
-        let a = $("<buttons>");
+        var a = $("<button>");
         a.addClass(classToAdd);
+        console.log(classToAdd)
         a.attr("data-type", arrayToUse[i]);
         a.text(arrayToUse[i]);
         $(areaToAddTo).append(a);
     }
 }
-$(document.onabort("click", ".food-button", function(){
+$(document).on ("click", ".food-button", function(){
     $("images").empty();
     $(".food.button").removeClass("active");
-    let type = $(this).attr("data.type");
-    let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=vBwyWQmFFPrM31T1RNrpkTF7zDoGBF1X";
+    let type = $(this).attr("data-type");
+    console.log(this)
+    let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=vBwyWQmFFPrM31T1RNrpkTF7zDoGBF1X&limit=10";
     $.ajax({
         url: queryURL,
-        metho: "GET"
+        method: "GET"
     })
     .then(function(response){
-        let results = response.data;
-        for ( var i =0; i < resutls.length; i++){
+        var results = response.data;
+        console.log (results)
+        for ( var i =0; i < results.length; i++){
             let foodDiv = $("<div class=\"food-item\">");
-            let rating = results [i].rating;
+            let rating = results[i].rating;
             let p = $("<p>"). text("Rating:" + rating);
-            let animated = results [i].images.fixed_height.url;
-            let still = resutls[i].images.fixed_height.url;
+            let animated = results[i].images.fixed_height.url;
+            let still = results[i].images.fixed_height_still.url;
             let foodImage = $("<img>");
-            foodImage.attr("src,still");
+            foodImage.attr("src",still);
             foodImage.attr("data-still", still);
-            foodImage.attr("data-animated", animated);
-            foodImage.attr("data-state","still");
-            fooodImage.addClass("food-image");
+            foodImage.attr("data-animate", animated);
+            foodImage.attr("data-state",still);
+            foodImage.addClass("food-image");
 
             foodDiv.append(p);
             foodDiv.append(foodImage);
@@ -41,16 +46,17 @@ $(document.onabort("click", ".food-button", function(){
             $("#images").append(foodDiv);
         }
     })
-}))
+})
 
 $(document).on("click", ".food-image", function(){
     let state = $(this).attr("data-state");
+    console.log(state)
     if (state === "still"){
-        $(this).attr("src", $(this).attr("data.animate"));
+        $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
     }
     else {
-        $(this).attr("src", $(this).attr("data-still");
+        $(this).attr("src", $(this).attr("data-still"));
         
     }
 });
